3447a5689480e559a081493a35ccca3a9a1a8cfc
Arijeet10 <arijeetsocial@gmail.com>
2024-03-31 09:58:12 +0530

    portfolio landing page

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -5,7 +5,7 @@ const HeroBanner = ({ heroData }) => {
         <img
           src={heroData.alternateAvatars[0].url}
           alt="profile image"
-          className=" rounded-full overflow-hidden"
+          className=" rounded-full"
         />
         <div className="absolute bottom-[75px] -left-[15px] sm:bottom-[150px] sm:-left-[5px] flex items-center justify-center gap-2 px-4 py-2 bg-white border border-black rounded-full">
           <span className="flex items-start text-5xl">
