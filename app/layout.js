a614ef6a8e82296c26fa805e77ec86f47c6833f7
Arijeet10 <arijeetsocial@gmail.com>
2024-03-31 21:11:02 +0530

    portfolio landing page

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=" bg-red-50">
+      <body className=" bg-red-50 p-2">
       <Navbar />
       {children}
       <Footer />
