7d783cc8ac9a0685da822f56d0d6a26a00d2bc2e
Arijeet10 <arijeetsocial@gmail.com>
2024-03-31 09:39:44 +0530

    portfolio landing page

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,8 +17,8 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <div className="flex items-center justify-between">
+    <div className="sticky top-0 ">
+      <div className=" flex items-center justify-between">
         <div className="text-5xl font-extrabold flex items-center">
           SARKAR <span className="text-green-500 text-base">○</span>
         </div>
@@ -31,25 +31,48 @@ const Navbar = () => {
         </div>
       </div>
       {navigation && (
-        <div className="flex flex-col gap-20 z-50 absolute top-0 right-0 h-full w-2/4 sm:w-[500px] px-4 py-8 bg-white ">
-        <div className=" h-36 flex items-start justify-end">
-        <button onClick={closeNavbar} className="">
-          <img src="/close.png" alt="close icon" className="w-10 h-10" />
-        </button>
+        <div className="flex flex-col gap-20 z-50 absolute top-0 right-0 h-screen w-2/4 sm:w-[500px] px-4 py-8 bg-white ">
+          <div className=" h-36 flex items-start justify-end">
+            <button onClick={closeNavbar} className="">
+              <img src="/close.png" alt="close icon" className="w-10 h-10" />
+            </button>
+          </div>
+          <div className="flex justify-center gap-8">
+            <div className="flex flex-col items-center">
+              <div className="border-2 border-black rounded-full p-4" />
+              <div className="border-l-2 border-black h-full" />
+              <div className="border-2 border-black rounded-full p-4" />
+            </div>
+            <div className="text-2xl font-bold flex flex-col items-center justify-center gap-4">
+              <Link href="/" className="hover:text-green-500">
+                HOME
+              </Link>
+              <Link href="#about" className="hover:text-green-500">
+                ABOUT
+              </Link>
+              <Link href="#services" className="hover:text-green-500">
+                SERVICES
+              </Link>
+              <Link href="#skills" className="hover:text-green-500">
+                SKILLS
+              </Link>
+              <Link href="#projects" className="hover:text-green-500">
+                PROJECTS
+              </Link>
+              <Link href="#timeline" className="hover:text-green-500">
+                TIMELINE
+              </Link>
+              <Link href="#testimonials" className="hover:text-green-500">
+                TESTIMONIALS
+              </Link>
+              <Link href="#contact" className="hover:text-green-500">
+                CONTACT
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="text-2xl font-bold flex flex-col items-center justify-center gap-4">
-        <Link href="/" className="hover:text-green-500">HOME</Link>
-        <Link href="#about" className="hover:text-green-500">ABOUT</Link>
-        <Link href="#services" className="hover:text-green-500">SERVICES</Link>
-        <Link href="#skills" className="hover:text-green-500">SKILLS</Link>
-        <Link href="#projects" className="hover:text-green-500">PROJECTS</Link>
-        <Link href="#timeline" className="hover:text-green-500">TIMELINE</Link>
-        <Link href="#testimonials" className="hover:text-green-500">TESTIMONIALS</Link>
-        <Link href="#contact" className="hover:text-green-500">CONTACT</Link>
-        </div>
-      </div>
       )}
-    </>
+    </div>
   );
 };
 
